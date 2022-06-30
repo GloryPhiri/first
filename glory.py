7a05810fdbd4e6cf406e3494f8a9cf1d44f4de46
GloryPhiri <108492942+GloryPhiri@users.noreply.github.com>
2022-06-30 22:38:21 +0200

    Add files via upload

---
+++ glory.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a = int(input('Enter any integer: '))
+b = int(input('Enter another number: '))
+print('The sum is '+ str(a+b))
+
+
+# In[ ]:
+
+
+
+
